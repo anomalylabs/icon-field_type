b1c6043fec7e7ad99546efb55118749a9d21aa19
Ryan Thompson <service@aiwebsystems.com>
2018-04-05 12:36:26 -0500

    Meh

--- resources/config/icons.php
+++ resources/config/icons.php
@@ -2,21 +2,24 @@
 
 return [
     'fontawesome' => [
-        'prefix' => 'fa fa-',
-        'name'   => 'Font Awesome',
-        'regex'  => '\.fa\-([a-z0-9-]+):before{',
-        'path'   => 'anomaly.field_type.icon::scss/fontawesome/font-awesome.scss',
+        'prefix'  => 'fa fa-',
+        'name'    => 'Font Awesome',
+        'regex'   => '\.fa\-([a-z0-9-]+):before{',
+        'website' => 'https://fontawesome.com/v4.7.0/icons/',
+        'path'    => 'anomaly.field_type.icon::scss/fontawesome/font-awesome.scss',
     ],
     'ionicons'    => [
-        'prefix' => 'icon ion-',
-        'name'   => 'Ionicons',
-        'regex'  => '\.ion\-([a-z0-9-]+):before{',
-        'path'   => 'anomaly.field_type.icon::scss/ionicons/ionicons.scss',
+        'prefix'  => 'icon ion-',
+        'name'    => 'Ionicons',
+        'website' => 'http://ionicons.com/',
+        'regex'   => '\.ion\-([a-z0-9-]+):before{',
+        'path'    => 'anomaly.field_type.icon::scss/ionicons/ionicons.scss',
     ],
-    'icomoon'    => [
-        'prefix' => 'ico icon-',
-        'name'   => 'IcoMoon',
-        'regex'  => '\.icon\-([a-z0-9-]+):before{',
-        'path'   => 'anomaly.field_type.icon::scss/icomoon/icomoon.scss',
+    'icomoon'     => [
+        'prefix'  => 'ico icon-',
+        'name'    => 'IcoMoon',
+        'regex'   => '\.icon\-([a-z0-9-]+):before{',
+        'website' => 'https://icomoon.io/#preview-free',
+        'path'    => 'anomaly.field_type.icon::scss/icomoon/icomoon.scss',
     ],
 ];
