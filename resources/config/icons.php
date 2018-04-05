6e0f29e75be656905f0cf2e9039c589e8d584eeb
Ryan Thompson <ryan@pyrocms.com>
2018-04-05 00:14:26 -0500

    Nice

--- resources/config/icons.php
+++ resources/config/icons.php
@@ -2,9 +2,10 @@
 
 return [
     'fontawesome' => [
-        'prefix' => 'fa fa-',
-        'name'   => 'Font Awesome',
-        'regex'  => '\.fa\-([a-z0-9-]+):before{',
-        'path'   => 'anomaly.field_type.icon::scss/fontawesome/font-awesome.scss',
+        'prefix'  => 'fa fa-',
+        'name'    => 'Font Awesome',
+        'regex'   => '\.fa\-([a-z0-9-]+):before{',
+        'website' => 'https://fontawesome.com/v4.7.0/icons/',
+        'path'    => 'anomaly.field_type.icon::scss/fontawesome/font-awesome.scss',
     ],
 ];
