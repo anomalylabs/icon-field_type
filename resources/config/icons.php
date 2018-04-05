8ff5ad31d2374dd270f74ca7867d6cbea7ac820f
Ryan Thompson <ryan@pyrocms.com>
2018-04-05 00:29:56 -0500

    Nice

--- resources/config/icons.php
+++ resources/config/icons.php
@@ -2,10 +2,15 @@
 
 return [
     'fontawesome' => [
-        'prefix'  => 'fa fa-',
-        'name'    => 'Font Awesome',
-        'regex'   => '\.fa\-([a-z0-9-]+):before{',
-        'website' => 'https://fontawesome.com/v4.7.0/icons/',
-        'path'    => 'anomaly.field_type.icon::scss/fontawesome/font-awesome.scss',
+        'prefix' => 'fa fa-',
+        'name'   => 'Font Awesome',
+        'regex'  => '\.fa\-([a-z0-9-]+):before{',
+        'path'   => 'anomaly.field_type.icon::scss/fontawesome/font-awesome.scss',
+    ],
+    'ionicons'    => [
+        'prefix' => 'icon ion-',
+        'name'   => 'Ionicons',
+        'regex'  => '\.ion\-([a-z0-9-]+):before{',
+        'path'   => 'anomaly.field_type.icon::scss/ionicons/ionicons.scss',
     ],
 ];
