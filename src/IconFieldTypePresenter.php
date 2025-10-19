8434937990e711bebf5134e767a8e0da135e582e
Aidan Watt <aidan@hotfootdesign.co.uk>
2025-10-19 12:18:10 +0100

    Laravel 12 work

--- src/IconFieldTypePresenter.php
+++ src/IconFieldTypePresenter.php
@@ -1,7 +1,7 @@
 <?php namespace Anomaly\IconFieldType;
 
 use Anomaly\Streams\Platform\Addon\FieldType\FieldTypePresenter;
-use Collective\Html\HtmlBuilder;
+use Anomaly\Streams\Platform\Html\HtmlBuilder;
 
 /**
  * Class IconFieldTypePresenter
